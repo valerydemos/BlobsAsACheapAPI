a9e512512faafc7c77e75df1256518eb3556489f
Valery Jacobs <valeryjacobs@hotmail.com>
2016-01-14 13:05:06 +0100

    Init

---
+++ ContentClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        private static void Start()
+        {
+            var client = new WebClient();
+
+            int i = 0;
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    var content = client.DownloadString("https://vjdemostorage.blob.core.windows.net/cheapapi/data.json");
+
+                    Console.WriteLine(i.ToString() + "\t\t" + content.Length.ToString());
+
+                    System.Threading.Thread.Sleep(5);
+                    i++;
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+    }
+}
---
+++ ContentManager/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace ContentManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=vjdemostorage;AccountKey=vwEq8bSfqCrz7usnTZ7UUpWe8f+PZRxkERBhMmpRHCan4LK8700SNIlTfZOjVSZ8N9VBHv+xtztaYcF1P4xHwg==;");
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("cheapapi");
+            container.CreateIfNotExists();
+            container.SetPermissions(
+            new BlobContainerPermissions
+            {
+                PublicAccess =
+            BlobContainerPublicAccessType.Blob
+            });
+
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("data.json");
+
+            // Create or overwrite the "myblob" blob with contents from a local file.
+            using (var fileStream = System.IO.File.OpenRead(@"c:\Files\data.json"))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+
+
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+
+                Console.WriteLine("Update the file and press a key.");
+                
+
+                using (var fileStream = System.IO.File.OpenRead(@"c:\Files\data.json"))
+                {
+                    blockBlob.UploadFromStream(fileStream);
+                }
+
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
